7381f99e0fd9e7fe0ed3317c9a4816ccbce3ffbc
hamzali <hamzali.tas.95@gmail.com>
2016-06-02 13:48:40 +0300

    added the objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,121 @@
+var bio = {
+    "name" : "Hamza Ali TAS",
+    "role" : "Software Devoloper",
+    "contacts" : {
+        "mobile" : "+905545077657",
+        "email" : "hamzalitas95@gmail.com",
+        "github" : "hamzali",
+        "twitter" : "@hamzali",
+        "location" : "Istanbul/Turkey"
+    },
+    "imageURL" : "images/profile.jpg",
+    "message" : "Hey dudes!",
+    "skills" : [
+        "love",
+        "magic",
+        "ice"
+    ]
+};
+
+var education = {
+    "schools" : [
+        {
+            "name" : "Istanbul Technical University",
+            "degree" : "BS",
+            "dates" : "2015 - 2020",
+            "location" : "Istanbul",
+            "majors" : ["Comp. eng."],
+            "url" : "https://www.itu.edu.tr"
+        },
+
+        {
+            "name" : "Selcuk University",
+            "degree" : "BS",
+            "dates" : "2013 - 2015",
+            "location" : "Konya",
+            "majors" : ["Comp. eng."],
+            "url" : "https://www.selcuk.edu.tr"
+        }
+    ],
+
+    "onlineClasses" : [
+        {
+            "title" : "JavaScript",
+            "school" : "Udacity",
+            "dates" : "10 march - 20 august",
+            "url" : "https://www.udacity.com"
+        },
+
+        {
+            "title" : "Python",
+            "school" : "Coursera",
+            "dates" : "15 may - 20 march",
+            "url" : "https://www.coursera.com"
+        }
+
+    ]
+};
+
+
+var projects = {
+    "projects" : [
+        {
+            "title" : "Race Game",
+            "dates" : "2013 - 2014",
+            "description" : "a race game in unity",
+            "image" : [
+                    "images/fry.jpg"
+            ]
+        }
+    ]
+};
+
+var work  = {
+    "jobs" : [
+        {
+            "employer" : "Pixar",
+            "title" : "Lead Graphics Programmer",
+            "location" : "California",
+            "dates" : "2017 - 2020",
+            "description" : "Lovely awesome job"
+
+        }
+    ]
+};
+
+
+
+
+
+
+
+
+
+
+
 /*
-This is empty on purpose! Your code to build the resume will go here.
- */
+formatedName = HTMLheaderName.replace("%data%", bio.name);
+formatedRole = HTMLheaderRole.replace("%data%", bio.role);
+image = HTMLbioPic.replace("%data%", bio.imageURL);
+formatedMsg = HTMLwelcomeMsg.replace("%data%", bio.message);
+// contacts info
+formatedmobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+formatedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+formatedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+formatedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+formatedLoc = HTMLlocation.replace("%data%", bio.contacts.location);
+// ~
+formatedSkills = HTMLskills.replace("%data%", bio.skills.join(' '));
+
+$("#header").prepend(formatedSkills);
+$("#header").prepend(formatedLoc);
+$("#header").prepend(formatedTwitter);
+$("#header").prepend(formatedGithub);
+$("#header").prepend(formatedEmail);
+$("#header").prepend(formatedmobile);
+$("#header").prepend(HTMLcontactGeneric);
+$("#header").prepend(formatedMsg);
+$("#header").prepend(image);
+$("#header").prepend(formatedRole);
+$("#header").prepend(formatedName);
+*/
